5b69156d52bd2b217c2a80c4c07e4df9b49dfc9c
Victor <barosv@yandex.ru>
2022-10-31 15:56:40 +0400

    на проверку

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,27 +1,3 @@
-// window.addEventListener('DOMContentLoaded', () => {
-
-//     const swiper = new Swiper('.swiper', {
-
-//         pagination: {
-//             el: '.promo__content-pagination',
-//             type: 'progressbar',
-//         },
-//         spaceBetween: 10,
-//         slidesPerView: 1,
-//     });
-
-//     let mySliderAllSlides = document.querySelector('.promo__content-total');
-//     let mySliderCurrentSlide = document.querySelector('.promo__content-current');
-//     let myImageSlider = document.querySelector('.promo__content-swiper');
-
-//     mySliderAllSlides.innerHTML = myImageSlider.slides.length;
-
-//     myImageSlider.on('slideChange', function () {
-//         let currentSlide = ++myImageSlider.realIndex;
-//         mySliderCurrentSlide.innerHTML = currentSlide;
-//     })
-
-// });
 window.addEventListener('DOMContentLoaded', () => {
     let totalSlide = ('.swiper-wrapper').length;
     const swiper = new Swiper('.promo__content-swiper', {
@@ -57,14 +33,22 @@ window.addEventListener('DOMContentLoaded', () => {
         fragment.innerHTML = (idx + '/' + tdx);
     });
 
+
+    // range
     const sumInputOne = document.querySelector('#input-one'),
-          sumInputTwo = document.querySelector('#input-two');
+        sumInputTwo = document.querySelector('#input-two'),
+        rangeInputSumOne = document.querySelector('#range__input-one'),
+        rangeInputSumTwo = document.querySelector('#range__input-two'),
+        rangeTrackSumOne = document.querySelector('#range__track-one'),
+        rangeTrackSumTwo = document.querySelector('#range__track-two');
 
-     
+    // маска
+    function prettify(num) {
+        const n = num.toString();
+        return n.replace(/(\d{1,3}(?=(?:\d\d\d)+(?!\d)))/g, "$1" + ' ');
+    }
 
-    function range(input, progress, content) {
-        const input$ = document.querySelector(input);
-        const progress$ = document.querySelector(progress);
+    function range(input$, progress$, content) {
         if (input$) {
             const val = input$.value;
             const min = input$.getAttribute('min');
@@ -91,13 +75,51 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    // маска
-    function prettify(num) {
-        var n = num.toString();
-        return n.replace(/(\d{1,3}(?=(?:\d\d\d)+(?!\d)))/g, "$1" + ' ');
-    }
+    range(rangeInputSumOne, rangeTrackSumOne, sumInputOne);
+    range(rangeInputSumTwo, rangeTrackSumTwo, sumInputTwo);
 
 
-    range('#range__input-one', '#range__track-one', sumInputOne);
-    range('#range__input-two', '#range__track-two', sumInputTwo);
+    sumInputOne.addEventListener('input', function () {
+
+        const minSum = rangeInputSumOne.getAttribute('min');
+        const maxSum = rangeInputSumOne.getAttribute('max');
+        const stepSum = rangeInputSumOne.getAttribute('step');
+
+        this.value = prettify(this.value.replace(/\D/g, ''))
+        if (+this.value.replace(/\D/g, '') > +maxSum) {
+            this.value = prettify(maxSum)
+            return
+        }
+        if (+this.value.replace(/\D/g, '') < +minSum) {
+            rangeInputSumOne.value = 0
+            rangeTrackSumOne.style.width = 0 + '%'
+            return
+        }
+        if (+this.value.replace(/\D/g, '') >= +minSum && +this.value.replace(/\D/g, '') <= +maxSum) {
+            rangeTrackSumOne.style.width = `${100 / (maxSum - stepSum) * (this.value.replace(/\D/g, '') - stepSum)}%`;
+            rangeInputSumOne.value = this.value.replace(/\D/g, '')
+        }
+    });
+
+    sumInputTwo.addEventListener('input', function () {
+
+        const minSum = rangeInputSumTwo.getAttribute('min');
+        const maxSum = rangeInputSumTwo.getAttribute('max');
+        const stepSum = rangeInputSumTwo.getAttribute('step');
+
+        this.value = prettify(this.value.replace(/\D/g, ''))
+        if (+this.value.replace(/\D/g, '') > +maxSum) {
+            this.value = prettify(maxSum)
+            return
+        }
+        if (+this.value.replace(/\D/g, '') < +minSum) {
+            rangeInputSumTwo.value = 0
+            rangeTrackSumTwo.style.width = 0 + '%'
+            return
+        }
+        if (+this.value.replace(/\D/g, '') >= +minSum && +this.value.replace(/\D/g, '') <= +maxSum) {
+            rangeTrackSumTwo.style.width = `${100 / (maxSum - stepSum) * (this.value.replace(/\D/g, '') - stepSum)}%`;
+            rangeInputSumTwo.value = this.value.replace(/\D/g, '')
+        }
+    })
 });
